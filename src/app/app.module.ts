9a986fc9d38f6ecd1a7afb09750e776b7a416209
DamianDubra <dubra.damian@gmail.com>
2023-04-10 11:01:30 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+
+
 
 @NgModule({
   declarations: [
@@ -10,6 +15,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
