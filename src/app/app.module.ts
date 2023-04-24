b6414f30498d184290708f383d2a7a44950f0fa1
DamianDubra <dubra.damian@gmail.com>
2023-04-24 11:07:30 -0300

    prueba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import 'zone.js';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -6,19 +7,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { MenuComponent } from './menu/menu.component';
+import 'zone.js';
+import { NgZone } from '@angular/core';
+
+
+
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ NgZone ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
